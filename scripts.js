0c81604e9581113b1178b1913623459b219fe666
J Brodie Day <jbrodieday@gmail.com>
2024-04-16 13:53:45 -0400

    logic(projects): refactor addProjects and make repoUrl conditional

--- scripts.js
+++ scripts.js
@@ -202,71 +202,71 @@ const projects = [
 ]
 
 // Create necessary elements for each project, populate, and append.
-function addProjects(array) {
-    for (let i = 0; i < array.length; i++) {
+const addProjects = (projects) => {
+  projects.map((project) => {
+    // Each project is an article that sits inside this projectsDiv.
+    const projectsDiv = document.getElementById('allProjects')
 
-        // Each project is an article that sits inside this projectsDiv.
-        const projectsDiv = document.getElementById('allProjects')
+    // Each project is contained in an article with individual class for that project's colour scheme.
+    const article = document.createElement('article');
+    article.classList = `projectContainer ${project.accentColorClass}`;
+    
+    // Project copy is a flex container containing heading, descriptive text, and a div containing live and repo links. 
+    const copyContainer = document.createElement('div');
+    copyContainer.classList = 'projectCopy';
 
-        // Each project is contained in an article with individual class for that project's colour scheme.
-        const article = document.createElement('article');
-        article.classList = `projectContainer ${array[i].accentColorClass}`;
-        
-        // Project copy is a flex container containing heading, descriptive text, and a div containing live and repo links. 
-        const copyContainer = document.createElement('div');
-        copyContainer.classList = 'projectCopy';
-
-        // Project heading
-        const heading = document.createElement('h3');
-        heading.textContent = array[i].title;
-        
-        // Project copy
-        const copyText = document.createElement('p');
-        copyText.innerText = array[i].copy;
-        
-        // Anchors for project urls
-        const linkDiv = document.createElement('div')
-        linkDiv.classList = 'projectLinks'
-            // Live url
-        const liveLink = document.createElement('a')
-        liveLink.setAttribute('href', array[i].liveUrl)
-        liveLink.textContent = 'Live'
-            // Repo url
-        const repoLink = document.createElement('a')
-        repoLink.setAttribute('href', array[i].repoUrl)
-        repoLink.textContent = 'Repo'
-            // Place them in the div.
-        linkDiv.appendChild(liveLink)
-        linkDiv.appendChild(repoLink)
-
-        // Place heading, copy, and links container inside the copy container.
-        copyContainer.appendChild(heading)
-        copyContainer.appendChild(copyText)
-        copyContainer.appendChild(linkDiv)
-
-        // Place whole copy container, with all of above, in the project article. 
-        article.appendChild(copyContainer)
-
-        // Project image and dotted accent border is in a div, placed in the article.
-        const imgDiv = document.createElement('div')
-            // Image and accent border
-        const accentDiv = document.createElement('div')
-        const projectImg = document.createElement('img')
-        imgDiv.classList = 'projectImg'
-        accentDiv.classList = 'projectAccent'
-        projectImg.setAttribute('src', array[i].imgUrl)
-        projectImg.setAttribute('alt', array[i].imgAlt)
-
-        // Place the image and accent in the image container.
-        imgDiv.appendChild(accentDiv)
-        imgDiv.appendChild(projectImg)
-
-        // Place image container in the project article.
-        article.appendChild(imgDiv)
-
-        // Finally, append article to the projects container.
-        projectsDiv.appendChild(article)
+    // Project heading
+    const heading = document.createElement('h3');
+    heading.textContent = project.title;
+    
+    // Project copy
+    const copyText = document.createElement('p');
+    copyText.innerText = project.copy;
+    
+    // Anchors for project urls
+    const linkDiv = document.createElement('div')
+    linkDiv.classList = 'projectLinks'
+      // Live url
+    const liveLink = document.createElement('a')
+    liveLink.setAttribute('href', project.liveUrl)
+    liveLink.textContent = 'Live'
+    linkDiv.appendChild(liveLink)
+    // Repo url - if the property exists
+    if (project.repoUrl) {
+      const repoLink = document.createElement('a')
+      repoLink.setAttribute('href', project.repoUrl)
+      repoLink.textContent = 'Repo'
+      linkDiv.appendChild(repoLink)
     }
+
+    // Place heading, copy, and links container inside the copy container.
+    copyContainer.appendChild(heading)
+    copyContainer.appendChild(copyText)
+    copyContainer.appendChild(linkDiv)
+
+    // Place whole copy container, with all of above, in the project article. 
+    article.appendChild(copyContainer)
+
+    // Project image and dotted accent border is in a div, placed in the article.
+    const imgDiv = document.createElement('div')
+    // Image and accent border
+    const accentDiv = document.createElement('div')
+    const projectImg = document.createElement('img')
+    imgDiv.classList = 'projectImg'
+    accentDiv.classList = 'projectAccent'
+    projectImg.setAttribute('src', project.imgUrl)
+    projectImg.setAttribute('alt', project.imgAlt)
+
+    // Place the image and accent in the image container.
+    imgDiv.appendChild(accentDiv)
+    imgDiv.appendChild(projectImg)
+
+    // Place image container in the project article.
+    article.appendChild(imgDiv)
+
+    // Finally, append article to the projects container.
+    projectsDiv.appendChild(article)
+  })
 }
 
 addProjects(projects)
