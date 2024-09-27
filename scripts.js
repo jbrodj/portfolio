fd6ec130f8c2492759f34f85ca312a355d355db3
J Brodie Day <jbrodieday@gmail.com>
2024-09-27 15:17:21 -0400

    feat(vitest): wrap main script in domcontentloaded listener for vitest

--- scripts.js
+++ scripts.js
@@ -1,26 +1,21 @@
 import { projects, skillIcons } from "./content.js"
 import { addSkillIcons, setCurrentLearningTopic, addProjects } from "./helpers.js"
 
-// ====== INJECT SKILL ICONS FOR GROWTH SECTION ======
-
-// Loop and print an icon for each index in the icons object.
-skillIcons.forEach(skill => addSkillIcons(skill))
-
-// ==========================================
+window.addEventListener('DOMContentLoaded', function() {
+  
+  // ====== INJECT PROJECT DATA FOR PROJECTS SECTION ======
+  addProjects(projects)
 
 
-// ====== INJECT CURRENT LEARNING ICON ======
-
-let currentTopic = 'Python' // <-- Set current topic here
-let currentDescription = '{ Building a full-stack app with Python, Flask, and SQL }' // <-- Set description here
-
-setCurrentLearningTopic(skillIcons, currentTopic, currentDescription)
-
-// ======================================================
-
-
-// ====== INJECT PROJECT DATA FOR PROJECTS SECTION ======
-
-addProjects(projects)
-
-// 
+  // ====== INJECT SKILL ICONS FOR GROWTH SECTION ======
+  // Loop and print an icon for each index in the icons object.
+  addSkillIcons(skillIcons)
+  
+  
+  // ====== INJECT CURRENT LEARNING ICON ======
+  let currentTopic = 'Python' // <-- Set current topic here
+  let currentDescription = '{ Building a full-stack app with Python, Flask, and SQL }' // <-- Set description here
+  
+  setCurrentLearningTopic(skillIcons, currentTopic, currentDescription)
+  
+}, false)
