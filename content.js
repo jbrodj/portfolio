694584db9a012403d683fccb10882a9e1faa5c83
J Brodie Day <jbrodieday@gmail.com>
2025-04-23 11:39:43 -0400

    fix(content): rearrange learning icons and fix projects copy

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ export const projects = [
       repoUrl: 'https://github.com/jbrodj/take-a-hike',
       imgUrl: './assets/take-a-hike.png',
       imgAlt: 'Screenshot of the Take a Hike "my hikes" view, showing location and distance information about a recent hike.',
-      copy: `Full stack social media application for sharing information about hikes and following other users to keep up with their activity! Serving user-generated content with a Python server with Flask, using SQLite for databases, JavaScript for UI interactivity, and a Cloudinary integration for image hosting. Tested with Pytest, using Github Actions for automated lint and unit testing.`,
+      copy: `Full-stack social media application for sharing hikes and following other users to keep up with their activity! Serving user-generated content with a Python server with Flask, using SQLite for databases, JavaScript for UI interactivity, and a Cloudinary integration for image hosting. Tested with Pytest, using Github Actions for automated lint and unit testing.`,
       accentColorClass: 'takeAHike'
   },
   {
@@ -14,7 +14,7 @@ export const projects = [
       liveUrl: 'https://www.telus.com/',
       imgUrl: './assets/telusMyTelusView.png',
       imgAlt: 'Screenshot of the My Telus App overview page.',
-      copy: `Full stack universal experiences for authenticated accounts in the My Telus app - including customer data API, app navigation, universal UI elements, and app-wide end-to-end. Built in React, Express & Node. Tested with Jest, Cypress, and Postman using Github Actions for automated runs. Using Docker for containerization; deployed on GKE and monitored with Dynatrace.`,
+      copy: `Full-stack universal experiences for authenticated accounts in the My Telus app - including customer data API, app navigation, universal UI elements, and app-wide end-to-end. Built in React, Express & Node. Tested with Jest, Cypress, and Postman using Github Actions for automated runs. Using Docker for containerization; deployed on GKE and monitored with Dynatrace.`,
       accentColorClass: 'telus'
   },
   {
@@ -39,6 +39,38 @@ export const projects = [
 // Title for tooltip; class name from devicon classes (appended to 'devicon-'). From https://devicon.dev/ 
 export const skillIcons = [
   {
+    title: 'JavaScript ES6+',
+    class: 'javascript-plain'
+  },
+  {
+      title: 'Python',
+      class: 'python-plain'
+  },
+  {
+      title: 'TypeScript',
+      class: 'typescript-plain'
+  },
+  {
+      title: 'C',
+      class: 'c-plain'
+  },
+  {
+    title: 'SQL',
+    class: 'sqlite-plain'
+  },
+  {
+    title: 'NodeJS',
+    class: 'nodejs-plain'
+  },
+  {
+    title: 'React',
+    class: 'react-original'
+  },
+  {
+      title: 'Flask',
+      class: 'flask-original'
+  },
+  {
       title: 'HTML5',
       class: 'html5-plain', 
   },
@@ -47,36 +79,12 @@ export const skillIcons = [
       class: 'css3-plain'
   },
   {
-      title: 'JavaScript ES6+',
-      class: 'javascript-plain'
-  },
-  {
-      title: 'React',
-      class: 'react-original'
-  },
-  {
-      title: 'Sass',
+    title: 'Sass',
       class: 'sass-original'
   },
   {
-      title: 'C',
-      class: 'c-plain'
-  },
-  {
-      title: 'Python',
-      class: 'python-plain'
-  },
-  {
-      title: 'Flask',
-      class: 'flask-original'
-  },
-  {
-      title: 'SQL',
-      class: 'sqlite-plain'
-  },
-  {
-      title: 'NodeJS',
-      class: 'nodejs-plain'
+      title: 'Bootstrap',
+      class: 'bootstrap-plain'
   },
   {
       title: 'NPM',
@@ -129,13 +137,5 @@ export const skillIcons = [
   {
       title: 'Confluence',
       class: 'confluence-plain'
-  },
-  {
-      title: 'MongoDB',
-      class: 'mongodb-plain'
-  },
-  {
-      title: 'Bootstrap',
-      class: 'bootstrap-plain'
   }
 ]
